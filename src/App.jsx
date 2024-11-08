85300a7a473f77429cabd3637dc7250c0598153a
Divya Shrivastava <shridivya893@gmail.com>
2024-11-08 23:12:53 +0530

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.js
 
 import React from 'react';
-import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import NewArrivals from './components/NewArrivals';
 import './index.css';
@@ -9,7 +8,6 @@ import './index.css';
 function App() {
   return (
     <div className="App">
-      <Header />
       <HeroSection />
       <NewArrivals />
     </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,25 +2,42 @@
 
 import React from 'react';
 import './HeroSection.css';
-
+import logo from '../assets/logo.png'; 
+import cart_icon from '../assets/cart_icon.png'
 function HeroSection() {
   return (
-    <section className="hero">
-      <div className="hero-content">
-        <h2>Newly Launched</h2>
-        <h1>Kryo X26 MTB</h1>
-        <p>Specifications:</p>
-        <ul>
-          <li>Lightweight 18" Frame</li>
-          <li>Steel Suspension Fork</li>
-          <li>Steel Hardtail Frame</li>
-        </ul>
-        <button className="buy-now">Buy Now</button>
-      </div>
-      <div className="hero-image">
-        <img src="https://via.placeholder.com/600x400" alt="Kryo X26 MTB" />
-      </div>
-    </section>
+    <div className="hero-container"> 
+      <header className="header">
+        <nav className="navbar">
+          <div className="logo"> <img width="120px" src={logo} alt="" /></div>
+          <ul className="nav-links">
+            <li>HOME</li>
+            <li>BICYCLES</li>
+            <li>ACCESSORIES</li>
+            <li>ABOUT US</li>
+            <li>CONTACT</li>
+          </ul>
+          <div className="cart">
+            <span>$0.00</span>
+            <img className="nav-login-cart" src={cart_icon} alt="Cart icon" />
+            <div className="nav-cart-count">0</div>
+          </div>
+        </nav>
+      </header>
+      <section className="hero">
+        <div className="hero-content">
+          <h2>Newly Launched</h2>
+          <h1>Kryo X26 MTB</h1>
+          <p>Specifications:</p>
+          <ul className='list'>
+            <li>Lightweight 18" Frame</li>
+            <li>Steel Suspension Fork</li>
+            <li>Steel Hardtail Frame</li>
+          </ul>
+          <button className="buy-now">BUY NOW</button>
+        </div>
+      </section>
+    </div>
   );
 }
 

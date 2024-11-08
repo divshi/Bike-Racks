da469b6d3d6fbf5c5def1923b6aba5d4bd30418d
Divya Shrivastava <shridivya893@gmail.com>
2024-11-09 02:11:21 +0530

    bottom part completed

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -3,12 +3,16 @@
 import React from 'react';
 import ProductCard from './ProductCard';
 import './NewArrivals.css';
+import cycle1 from '../assets/bicycle-7.jpg';
+import cycle2 from '../assets/bicycle-1.jpg';
+import cycle3 from '../assets/bicycle-5.jpg';
+import cycle4 from '../assets/bicycle-4.jpg';
 
 const products = [
-  { id: 1, name: 'Kryo X26 MTB - Model K', price: '$350.00' },
-  { id: 2, name: 'Kryo X26 MTB - Model X', price: '$350.00' },
-  { id: 3, name: 'Kryo X26 MTB - Model Y', price: '$350.00' },
-  { id: 4, name: 'Kryo X26 MTB - Model Z', price: '$350.00' },
+  { id: 1, name: 'Kryo X26 MTB - Model K', price: '$350.00', image: cycle1 },
+  { id: 2, name: 'Kryo X26 MTB - Model X', price: '$350.00', image: cycle2 }, 
+  { id: 3, name: 'Kryo X26 MTB - Model Y', price: '$350.00', image: cycle3 }, 
+  { id: 4, name: 'Kryo X26 MTB - Model Z', price: '$350.00', image: cycle4 }, 
 ];
 
 function NewArrivals() {
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,8 +6,10 @@ import './ProductCard.css';
 function ProductCard({ product }) {
   return (
     <div className="product-card">
-      <img src="https://via.placeholder.com/200x200" alt={product.name} />
+      <img src={product.image} alt={product.name} />
+      <h3 className='b'>Bicycles</h3>
       <h3>{product.name}</h3>
+      <h3 className='star'>☆☆☆☆☆</h3>
       <p>{product.price}</p>
     </div>
   );
